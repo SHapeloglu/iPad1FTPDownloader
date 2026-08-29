61437a6e17dd1aed863155d19feb50e267b6f708
SHapeloglu <154635217+SHapeloglu@users.noreply.github.com>
2026-08-29 18:31:53 +0300

    Add lightweight remote search and sort state

--- src/AppDelegate.h
+++ src/AppDelegate.h
@@ -3,10 +3,32 @@
 #import "FTPUploader.h"
 #import "FTPBrowser.h"
 #import "FTPCommandClient.h"
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate,FTPDownloaderDelegate,FTPUploaderDelegate,FTPBrowserDelegate,FTPCommandClientDelegate,UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate,UIActionSheetDelegate,UIAlertViewDelegate> {
- UIWindow *_window; UITextField *_hostField,*_portField,*_userField,*_passField,*_pathField; UIButton *_connectButton,*_upButton,*_serversButton,*_saveButton,*_uploadButton,*_newFolderButton; UILabel *_statusLabel,*_bytesLabel; UIProgressView *_progressView; UITableView *_tableView;
- FTPDownloader *_downloader; FTPUploader *_uploader; FTPBrowser *_browser; FTPCommandClient *_commandClient; NSArray *_items; NSString *_currentPath,*_host,*_username,*_password; NSInteger _port; unsigned long long _downloadExpected; NSTimeInterval _downloadStart;
- UIActionSheet *_serversSheet,*_uploadSheet,*_completionSheet; UIAlertView *_saveAlert,*_renameAlert,*_folderAlert; NSIndexPath *_pendingIndexPath; NSString *_completedDownloadPath;
+ UIWindow *_window;
+ UITextField *_hostField,*_portField,*_userField,*_passField,*_pathField,*_searchField;
+ UIButton *_connectButton,*_upButton,*_serversButton,*_saveButton,*_uploadButton,*_newFolderButton,*_sortButton;
+ UILabel *_statusLabel,*_bytesLabel;
+ UIProgressView *_progressView;
+ UITableView *_tableView;
+
+ FTPDownloader *_downloader;
+ FTPUploader *_uploader;
+ FTPBrowser *_browser;
+ FTPCommandClient *_commandClient;
+
+ NSArray *_items,*_allItems;
+ NSString *_currentPath,*_host,*_username,*_password;
+ NSInteger _port;
+ NSInteger _sortMode;
+ BOOL _foldersFirst;
+ unsigned long long _downloadExpected;
+ NSTimeInterval _downloadStart;
+
+ UIActionSheet *_serversSheet,*_uploadSheet,*_completionSheet,*_sortSheet;
+ UIAlertView *_saveAlert,*_renameAlert,*_folderAlert;
+ NSIndexPath *_pendingIndexPath;
+ NSString *_completedDownloadPath;
 }
 @property(nonatomic,retain)UIWindow *window;
 @end
