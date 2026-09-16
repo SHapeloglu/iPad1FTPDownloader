d107debb268d724d0ee86cd4008f7c5a8869c570
SHapeloglu <154635217+SHapeloglu@users.noreply.github.com>
2026-09-16 13:13:53 +0300

    fix: restore proven AppDelegate launch path

--- src/main.m
+++ src/main.m
@@ -1,9 +1,9 @@
 #import <UIKit/UIKit.h>
-#import "UnifiedAppDelegate.h"
+#import "AppDelegate.h"
 
 int main(int argc, char *argv[]) {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([UnifiedAppDelegate class]));
+    int retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     [pool drain];
     return retVal;
 }
